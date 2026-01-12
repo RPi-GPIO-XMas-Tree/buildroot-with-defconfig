d94b6555e1c46177812566ac880c4d8e28e8cfae
TrifanBogdan24 <trifanbogdan24@gmail.com>
2026-01-12 21:57:01 +0200

    Deactivated more driver and used kernel compression

--- cp-artifacts.sh
+++ cp-artifacts.sh
@@ -1,6 +1,8 @@
 #!/bin/sh
 
-cp buildroot/output/images/Image vmlinuz-tema2
+cp buildroot/output/images/Image.gz vmlinuz-tema2
 cp buildroot/output/images/bcm2837-rpi-3-b.dtb bcm2837-rpi-3-b.dtb
 cp buildroot/output/images/rootfs.ext4 tema2.img
 qemu-img resize tema2.img 64M
+
+
