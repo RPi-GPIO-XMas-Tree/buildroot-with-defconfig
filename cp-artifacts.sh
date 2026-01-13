67aec703375850649d530d6e7553bfba05954bdb
TrifanBogdan24 <trifanbogdan24@gmail.com>
2026-01-13 08:33:48 +0200

    tar rootfs with gz

--- cp-artifacts.sh
+++ cp-artifacts.sh
@@ -4,3 +4,4 @@ cp buildroot/output/images/Image.gz vmlinuz-tema2
 cp buildroot/output/images/bcm2837-rpi-3-b.dtb bcm2837-rpi-3-b.dtb
 cp buildroot/output/images/rootfs.ext4 tema2.img
 qemu-img resize tema2.img 8M
+
